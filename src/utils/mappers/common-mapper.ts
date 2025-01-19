68cb9499344e921137fe83de87a39010d4b960e1
DESKTOP-8T462RT\jerry <jerryblaauw@hotmail.com>
2025-01-19 03:01:08 +0100

    fixed the table styling

--- src/utils/mappers/common-mapper.ts
+++ src/utils/mappers/common-mapper.ts
@@ -17,17 +17,17 @@ export const mapRichText = (richText: any): string | undefined => {
       [BLOCKS.PARAGRAPH]: (node: any, next: any) =>
         `<p>${next(node.content).replace(/\n/g, "<br/>")}</p>`,
       [BLOCKS.TABLE]: (node: any, next: any) =>
-        `<div class="overflow-auto p-10"><table class="table-auto w-full">${next(
-          node.content
-        )}</table></div>`,
+        `<div class="relative flex flex-col overflow-scroll">
+          <table>
+            ${next(node.content)}
+          </table>
+        </div>`,
       [BLOCKS.TABLE_HEADER_CELL]: (node: any, next: any) =>
-        `<th class="px-6 py-2 font-medium text-left">${next(
-          node.content
-        )}</th>`,
+        `<th>${next(node.content)}</th>`,
+      [BLOCKS.TABLE_ROW]: (node: any, next: any) =>
+        `<tr>${next(node.content)}</tr>`,
       [BLOCKS.TABLE_CELL]: (node: any, next: any) =>
-        `<td class="border-t border-slate-700 px-6 py-2">${next(
-          node.content
-        )}</td>`,
+        `<td>${next(node.content)}</td>`,
     },
   };
 
